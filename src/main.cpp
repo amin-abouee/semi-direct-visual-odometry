6b9a4e2b53105c7aece5838d5f614adf8a1e20b4
Amin Abouee <Amin.Abouee@gmail.com>
2020-02-17 02:27:18 +0100

    easylogging added

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #include <iostream>
 
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 // #include <opencv2/core/eigen.hpp>
 
 #include <Eigen/Core>
@@ -17,22 +17,15 @@
 #include "system.hpp"
 #include "utils.hpp"
 
+// #include <spdlog/spdlog.h>
 // #include "spdlog/sinks/stdout_color_sinks.h"
-#include <spdlog/spdlog.h>
-
 #include <nlohmann/json.hpp>
-
+#include <easylogging++.h>
+INITIALIZE_EASYLOGGINGPP
 
 
 int main( int argc, char* argv[] )
 {
-    int * myPointer = new int(5);
-    std::shared_ptr<int> mySharedPtrA(myPointer);
-    std::cout << "mySharedPtrA: " << mySharedPtrA.use_count() << std::endl;
-    std::shared_ptr<int> mySharedPtrB = mySharedPtrA;
-    std::cout << "mySharedPtrB: " << mySharedPtrB.use_count() << std::endl;
-
-// System system();
 
 #ifdef EIGEN_MALLOC_ALREADY_ALIGNED
     std::cout << "EIGEN_MALLOC_ALREADY_ALIGNED" << std::endl;
@@ -56,6 +49,14 @@ int main( int argc, char* argv[] )
     std::cout << "EIGEN_HAS_CXX11_CONTAINERS: " << EIGEN_HAS_CXX11_CONTAINERS << std::endl;
     std::cout << "EIGEN_MAX_CPP_VER: " << EIGEN_MAX_CPP_VER << std::endl;
     std::cout << "EIGEN_HAS_CXX11_MATH: " << EIGEN_HAS_CXX11_MATH << std::endl;
+
+    el::Loggers::addFlag( el::LoggingFlag::MultiLoggerSupport );
+    el::Loggers::addFlag( el::LoggingFlag::ColoredTerminalOutput );
+    // el::Loggers::configureFromGlobal( logFileName.c_str() );
+    // el::Loggers::reconfigureAllLoggers(conf);
+    // el::Logger* systemLogger = el::Loggers::getLogger("system"); // Register new logger
+    el::Loggers::getLogger( "Main" );  // Register new logger
+    #define SYSTEM_LOG( LEVEL ) CLOG( LEVEL, "Main" )
     // std::cout << "Number of Threads: " << Eigen::nbThreads( ) << std::endl;
     // Eigen::setNbThreads(4);
     // std::cout << "Number of Threads: " << Eigen::nbThreads( ) << std::endl;
@@ -63,10 +64,12 @@ int main( int argc, char* argv[] )
     // auto mainLogger = spdlog::stdout_color_mt( "main" );
     // mainLogger->set_level( spdlog::level::debug );
     // mainLogger->set_pattern( "[%Y-%m-%d %H:%M:%S] [%s:%#] [%n->%l] [thread:%t] %v" );
+    // spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
     // mainLogger->info( "Info" );
     // mainLogger->debug( "Debug" );
     // mainLogger->warn( "Warn" );
     // mainLogger->error( "Error" );
+    SYSTEM_LOG( INFO ) << "Scale";
 
     std::string configIOFile;
     if ( argc > 1 )
@@ -76,38 +79,37 @@ int main( int argc, char* argv[] )
 
     // Config::init(utils::findAbsoluteFilePath( configIOFile ));
     // Config* config = Config::getInstance();
-    Config config (utils::findAbsoluteFilePath( configIOFile ));
-    System system(config);
+    Config config( utils::findAbsoluteFilePath( configIOFile ) );
+    System system( config );
 
     const cv::Mat refImg = cv::imread( utils::findAbsoluteFilePath( "input/0000000000.png" ), cv::IMREAD_GRAYSCALE );
     const cv::Mat curImg = cv::imread( utils::findAbsoluteFilePath( "input/0000000001.png" ), cv::IMREAD_GRAYSCALE );
 
- 
-    system.processFirstFrame(refImg);
- 
+    system.processFirstFrame( refImg );
+
     auto t1 = std::chrono::high_resolution_clock::now();
-    system.processSecondFrame(curImg);
+    system.processSecondFrame( curImg );
     auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << "Elapsed time for matching: " << std::chrono::duration_cast< std::chrono::microseconds >( t2 - t1 ).count() << " micro sec"
               << std::endl;
 
-/*
-    numObserves = refFrame.numberObservation();
-    Eigen::VectorXd newCurDepths( numObserves );
-    algorithm::depthCamera( curFrame, newCurDepths );
-    medianDepth           = algorithm::computeMedian( newCurDepths );
-    const double minDepth = newCurDepths.minCoeff();
-    // std::cout << "Mean: " << medianDepth << " min: " << minDepth << std::endl;
-    curFrame.setKeyframe();
-*/
+    /*
+        numObserves = refFrame.numberObservation();
+        Eigen::VectorXd newCurDepths( numObserves );
+        algorithm::depthCamera( curFrame, newCurDepths );
+        medianDepth           = algorithm::computeMedian( newCurDepths );
+        const double minDepth = newCurDepths.minCoeff();
+        // std::cout << "Mean: " << medianDepth << " min: " << minDepth << std::endl;
+        curFrame.setKeyframe();
+    */
 
-    for (int i(2); i< 10; i++)
+    for ( int i( 2 ); i < 10; i++ )
     {
         std::stringstream ss;
-        ss << "input/000000000" << i <<".png";
+        ss << "input/000000000" << i << ".png";
         std::cout << "filename: " << ss.str() << std::endl;
         cv::Mat newImg = cv::imread( utils::findAbsoluteFilePath( ss.str() ), cv::IMREAD_GRAYSCALE );
-        system.processNewFrame(newImg);
+        system.processNewFrame( newImg );
         cv::waitKey( 0 );
     }
     // system.reportSummaryFrames();
